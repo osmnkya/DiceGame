a7c0e53786c8edab835eacab9391c1380b9a5057
Osman K <55282005+osmnkya@users.noreply.github.com>
2021-09-26 23:40:40 +0200

    Add files via upload

---
+++ EmptyClass.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Kattis
+{
+    public static void Main(string args)
+    {
+        int[] dice1 = new int[4];
+        int[] dice2 = new int[4];
+
+        string[] line1;
+        line1 = Console.ReadLine().Split(' ');
+        string[] line2;
+        line2 = Console.ReadLine().Split(' ');
+
+        for (int i = 0; i < 4; ++i)
+        {
+            dice1[i] = int.Parse(line1[i]);
+            dice2[i] = int.Parse(line2[i]);
+        }
+
+        double[] result = new double[2];
+
+        double sum, t;
+
+        for (int i = 0; i < 4; ++i)
+        {
+            sum = 0;
+            t = 0;
+            for (int j = dice1[i]; j <= dice2[i]; ++j)
+            {
+                sum += j;
+                ++t;
+            }
+            result[i / 2] += sum / t;
+        }
+
+        if (result[1] > result[0])
+        {
+            Console.WriteLine("Emma");
+        }
+        else if (result[1] < result[0])
+        {
+            Console.WriteLine("Gunnar");
+        }
+        else
+        {
+            Console.WriteLine("Tie");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Kattis.DiceGame
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+                string[] emma = Console.ReadLine().Split(' ');
+                string[] gunnar = Console.ReadLine().Split(' ');
+                int sumEmma = 0;
+                int sumGunnar = 0;
+
+                for (int i = 0; i < emma.Length; i++)
+                {
+                    sumEmma += int.Parse(emma[i]);
+                }
+                for (int j = 0; j < gunnar.Length; j++)
+                {
+                    sumGunnar += int.Parse(gunnar[j]);
+                }
+
+                string whoWin = " ";
+
+                if (sumEmma < sumGunnar)
+                    whoWin = "Emma";
+                else if (sumGunnar < sumEmma)
+                    whoWin = "Gunnar";
+                else
+                    whoWin = "Tie";
+
+            Console.WriteLine(whoWin);
+        }
+    }
+}
